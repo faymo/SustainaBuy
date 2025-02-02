8060170fdd836a82fb7813c05a3861b0867ed707
arunt1130 <arun.thiruthanikan@gmail.com>
2025-02-02 00:56:18 -0500

    Fixed Prompt for Rating

--- my-extension/src/Gemini_API.js
+++ my-extension/src/Gemini_API.js
@@ -20,8 +20,8 @@ Use this url, ${url}`;
 export async function getSustainabilityScore(mass, material) {
   const aiPrompt = `
     Analyze the following product characteristics and find an exact Greenhouse gas emissions based specifically on the mass (larger mass means more emissions which is bad) and material given (kg). Generate a sustainability score from 0 to 10 (1 decimal place allowed), that considers each characteristic, where 10 is highly sustainable. Remember that materials like bamboo(0.5kg/1000g co2e/bamboomass) are more sustainable than plastic (2-6kg/1000g co2e/plasticmass). Remember that 10-100g is not that much compared to 1+ kg.
-    - Mass: 90 grams
-    - Material: Plastic
+    - Mass: ${mass}
+    - Material: ${material}
     In your response, INCLUDE YOUR SUSTAINABILITY SCORE between 0-10.0, and explain the reasoning. Your response should be in the format of this example: "(Sustainability Score), (Greenhouse Gas Emissions)kg,   Reasoning/explanation: ...." MAKE SURE THE SCORE/NUMBER IS THE FIRST THING IN YOUR RESPONSE, and dont add any asteriks or other characters
   `;
   const result = await model.generateContent(aiPrompt);
