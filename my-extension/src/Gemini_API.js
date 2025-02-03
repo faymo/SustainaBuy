9592e7369398a44cae290e21d990406f2e1a6175
aksh-13 <73313866+aksh-13@users.noreply.github.com>
2025-02-03 15:06:58 -0500

    Final changes

--- my-extension/src/Gemini_API.js
+++ my-extension/src/Gemini_API.js
@@ -1,15 +1,14 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI("AIzaSyC7xUkHVXyOMoDsvKhbYHl5C2lRW0YEsvo");
+const genAI = new GoogleGenerativeAI("AIzaSyCkHEGOS-dAhr38iT8dxH5OD4kKJ0bBhpo");
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 export async function getSustainableAlternatives(url) {
-  const prompt = `Use this url to look at the product, and give me a list of the name, price, and link of 3 sustainable alternatives of the same or similar product. Make sure the product is more sustainable, so look for things like what material it's made out of, and the mass. Also look at the carbon footprint of similar products and compare it to this link. Make sure you dont have '**' in your format and make sure your links work. Make sure you make it into this format.
+  const prompt = `Use this url to look at the product, and give me a list of the name, price, and link of only 3 sustainable alternatives of the same or similar product. Make sure the product is more sustainable, so look for things like what material it's made out of, and the mass. Also look at the carbon footprint of similar products and compare it to this link. Make sure you dont have '**' in your format and MAKE SURE YOUR LINKS WORK AND SHOW THEM EVERYTIME. MAKE SURE YOUR LINK WORKS AND HAVE A NUMBER FOR THE C02 EMISSIONS!!!! Make sure you make it into this format.
 Name: _____.
 Underneath that, Price.
 Underneath that, CarbonEmission.
-Make sure the link is VALID.
-Underneath that, the link.
+Underneath that, THE LINK.
 Use this url, ${url}`;
   
   const result = await model.generateContent(prompt);
